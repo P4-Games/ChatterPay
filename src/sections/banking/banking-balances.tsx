28d865dd169d1b7a9a1cc133810590d5d5e410dd
dappsar <dapps.ar@gmail.com>
2024-08-15 22:18:41 -0300

    [feat] :sparkles: show 0 char when no balance

--- src/sections/banking/banking-balances.tsx
+++ src/sections/banking/banking-balances.tsx
@@ -139,7 +139,7 @@ export default function BankingBalances({
   const renderTotal = (
     <Stack direction='row' alignItems='center' spacing={1} sx={{ mb: 2, px: 2, ml: 1 }}>
       <Typography variant='h3'>
-        {currency.value ? fNumber(tableData.totals[selectedCurrency]) : '********'}
+        {currency.value ? fNumber(tableData.totals[selectedCurrency]) || 0.0 : '********'}
       </Typography>
       <Select
         value={selectedCurrency ?? ''}
