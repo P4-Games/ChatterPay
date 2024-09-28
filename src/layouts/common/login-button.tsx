c6444b038c9b234046e6fd5963b620249ca1f7b7
dappsar <dapps.ar@gmail.com>
2024-09-28 03:15:41 -0300

    [feat] :sprakles: improve sign-in, sign-up buttons (#101)

--- src/layouts/common/login-button.tsx
+++ src/layouts/common/login-button.tsx
@@ -5,6 +5,7 @@ import { paths } from 'src/routes/paths'
 import { RouterLink } from 'src/routes/components'
 
 import { useTranslate } from 'src/locales'
+import { PATH_AFTER_LOGIN } from 'src/config-global'
 
 // ----------------------------------------------------------------------
 
@@ -16,13 +17,18 @@ export default function LoginButton({ sx }: Props) {
   const { t } = useTranslate()
 
   return (
-    <Button
-      component={RouterLink}
-      href={paths.auth.jwt.register}
-      variant='outlined'
-      sx={{ mr: 1, ...sx }}
-    >
-      {t('login.my-wallet')}
-    </Button>
+    <>
+      <Button
+        component={RouterLink}
+        href={paths.auth.jwt.register}
+        variant='contained'
+        sx={{ mr: 1, ...sx }}
+      >
+        {t('home.header.sign-up')}
+      </Button>
+      <Button component={RouterLink} href={PATH_AFTER_LOGIN} variant='soft' sx={{ mr: 1, ...sx }}>
+        {t('home.header.sign-in')}
+      </Button>
+    </>
   )
 }
