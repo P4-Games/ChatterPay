89f1311a5f6f5058fc55a891685be5aed0038bc8
dappsar <dapps.ar@gmail.com>
2025-01-20 16:15:24 -0300

    [fix] :green_heart: fix build error by new read of boolean env vars

--- src/config-global.ts
+++ src/config-global.ts
@@ -15,29 +15,28 @@ const {
   BOT_API_URL,
   BACKEND_API_URL,
   BACKEND_API_TOKEN,
-  BOT_API_WAPP_ENABLED = 'true',
+  BOT_API_WAPP_ENABLED: _botApiWappEnabled = 'true',
   NODE_PROVIDER_SEPOLIA_URL,
   NODE_PROVIDER_MUMBAI_URL,
   NODE_PROVIDER_SCROLL_URL,
   JWT_SECRET = '',
-  API3_ENABLED = 'false',
+  API3_ENABLED: _api3Enabled = 'false',
   DB_CHATTERPAY_NAME = 'chatterpay-dev',
   DB_BOT_NAME = 'chatterpay-develop',
   RECAPTCHA_API_KEY = '',
   PUSH_NETWORK = '11155111',
   PUSH_ENVIRONMENT = 'DEV',
-  HANDLE_VERCEL_FREE_PLAN_TIMEOUT = 'true',
+  HANDLE_VERCEL_FREE_PLAN_TIMEOUT: _handleVercelFreePlanTimeOut = 'true',
   NEXT_PUBLIC_UI_URL,
-  NEXT_PUBLIC_USE_MOCK = 'true',
+  NEXT_PUBLIC_USE_MOCK: _useMock = 'true',
   NEXT_PUBLIC_ALLOWED_ORIGINS = '*',
-  NEXT_PUBLIC_FROM_ICP = 'true',
+  NEXT_PUBLIC_FROM_ICP: _fromICP = 'true',
   NEXT_PUBLIC_RECAPTCHA_SITE_KEY = '',
   NEXT_PUBLIC_EXPLORER_L1_URL = 'https://sepolia.etherscan.io',
   NEXT_PUBLIC_EXPLORER_L2_URL = 'https://sepolia.arbiscan.io',
   NEXT_PUBLIC_EXPLORER_NFT_URL = 'https://sepolia.arbiscan.io',
   NEXT_PUBLIC_NFT_IMAGE_REPOSITORY = 'gcp'
 } = process.env
-
 
 // ----------------------------------------------------------------------
 // environment: set server-side
@@ -50,7 +49,6 @@ export {
   MONGODB_BOT,
   BOT_API_URL,
   DB_BOT_NAME,
-  API3_ENABLED,
   PUSH_NETWORK,
   BOT_API_TOKEN,
   BACKEND_API_URL,
@@ -59,7 +57,8 @@ export {
   DB_CHATTERPAY_NAME
 }
 
-export const botApiWappEnabled = BOT_API_WAPP_ENABLED === 'true'
+export const API3_ENABLED: boolean = _api3Enabled.toLowerCase() === 'true'
+export const botApiWappEnabled: boolean = _botApiWappEnabled.toLowerCase() === 'true'
 export const nodeProviderUrlSepolia = NODE_PROVIDER_SEPOLIA_URL
 export const nodeProviderUrlPolygon = NODE_PROVIDER_MUMBAI_URL
 export const nodeProviderUrlScroll = NODE_PROVIDER_SCROLL_URL
@@ -68,16 +67,17 @@ export const PUSH_ENVIRONMENT_PARSED: ENV = PUSH_ENVIRONMENT as ENV
 // Vercel has a timeout of 10 seconds (only for free plan) in the APIs.
 // The login has certain logic between ChatterPay and the backend of the Chatizalo,
 // which may cause it to take about 10 seconds, so this variable is used to improve that logic.
-export const handleVercelFreePlanTimeOut = HANDLE_VERCEL_FREE_PLAN_TIMEOUT === 'true'
+export const handleVercelFreePlanTimeOut: boolean =
+  _handleVercelFreePlanTimeOut.toLowerCase() === 'true'
 // ----------------------------------------------------------------------
 
 // ----------------------------------------------------------------------
 // environment: set client-side
 // ----------------------------------------------------------------------
 export const UI_API_URL = NEXT_PUBLIC_UI_URL
-export const USE_MOCK = NEXT_PUBLIC_USE_MOCK === 'true'
+export const USE_MOCK: boolean = _useMock.toLowerCase() === 'true'
 export const ALLOWED_ORIGINS = NEXT_PUBLIC_ALLOWED_ORIGINS
-export const fromICP = NEXT_PUBLIC_FROM_ICP === 'true'
+export const fromICP: boolean = _fromICP.toLowerCase() === 'true'
 export const RECAPTCHA_SITE_KEY = NEXT_PUBLIC_RECAPTCHA_SITE_KEY
 export const EXPLORER_L1_URL = NEXT_PUBLIC_EXPLORER_L1_URL
 export const EXPLORER_L2_URL = NEXT_PUBLIC_EXPLORER_L2_URL
