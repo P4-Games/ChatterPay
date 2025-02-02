6bcb5d23d8611b9cbf6ef3c920a06db150fdd943
dappsar <dapps.ar@gmail.com>
2025-02-02 04:14:23 -0300

    [fix] :lock: improve security in wallet/balance endpoint (#183)

--- src/app/api/v1/wallet/[id]/balance/route.ts
+++ src/app/api/v1/wallet/[id]/balance/route.ts
@@ -1,6 +1,9 @@
-import { NextResponse } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server'
 
+import { getIpFromRequest } from 'src/app/api/_utils/request-utils'
 import { defaultBalance, GET_BALANCES_FROM_BACKEND } from 'src/config-global'
+import { JwtPayload, extractjwtTokenFromHeader } from 'src/app/api/_utils/jwt-utils'
+import { getUserIdByWallet, checkUserHaveActiveSession } from 'src/app/api/_data/data-service'
 import {
   getBalancesWithTotals,
   getBalancesWithTotalsFromBackend
@@ -17,7 +20,7 @@ type IParams = {
 
 // ----------------------------------------------------------------------
 
-export async function GET(request: Request, { params }: { params: IParams }) {
+export async function GET(req: NextRequest, { params }: { params: IParams }) {
   if (!params.id) {
     const errorMessage: IErrorResponse = {
       error: {
@@ -25,7 +28,7 @@ export async function GET(request: Request, { params }: { params: IParams }) {
         message: `Wallet id '${params.id}' not found`,
         details: '',
         stack: '',
-        url: request.url
+        url: req.url
       }
     }
     return new NextResponse(JSON.stringify(errorMessage), {
@@ -37,6 +40,46 @@ export async function GET(request: Request, { params }: { params: IParams }) {
   // avoid slow context-user load issues
   if (params.id === 'none') {
     return NextResponse.json([defaultBalance])
+  }
+
+  const userId: string | undefined = await getUserIdByWallet(params.id)
+  if (!userId) {
+    return new NextResponse(
+      JSON.stringify({
+        code: 'USER_NOT_FOUND',
+        error: `user not found with for wallet ${params.id}`
+      }),
+      {
+        status: 404,
+        headers: { 'Content-Type': 'application/json' }
+      }
+    )
+  }
+
+  const ip = getIpFromRequest(req)
+
+  const jwtTokenDecoded: JwtPayload | null = extractjwtTokenFromHeader(
+    req.headers.get('Authorization')
+  )
+  if (!jwtTokenDecoded) {
+    return new NextResponse(
+      JSON.stringify({ code: 'NOT_AUTHORIZED', error: 'Invalid Access Token' }),
+      {
+        status: 401,
+        headers: { 'Content-Type': 'application/json' }
+      }
+    )
+  }
+
+  const validAccessToken = await checkUserHaveActiveSession(userId, jwtTokenDecoded, ip)
+  if (!validAccessToken) {
+    return new NextResponse(
+      JSON.stringify({ code: 'NOT_AUTHORIZED', error: 'Invalid Access Token' }),
+      {
+        status: 401,
+        headers: { 'Content-Type': 'application/json' }
+      }
+    )
   }
 
   try {
