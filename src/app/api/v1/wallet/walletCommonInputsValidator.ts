485769d7aa51026681c3c06eb38bc1664316d0aa
dappsar <dapps.ar@gmail.com>
2025-02-03 23:14:47 -0300

    [feat] :alien: add wallet-requests common input validators

---
+++ src/app/api/v1/wallet/walletCommonInputsValidator.ts
@@ -0,0 +1,36 @@
+import { NextRequest, NextResponse } from 'next/server'
+
+import { getUserIdByWallet } from 'src/app/api/_data/data-service'
+
+import { IErrorResponse } from 'src/types/api'
+
+export async function validateWalletCommonsInputs(
+  req: NextRequest,
+  walletId?: string
+): Promise<{ walletId: string; userId: string } | NextResponse> {
+  if (!walletId) {
+    const errorMessage: IErrorResponse = {
+      error: {
+        code: 'INVALID_REQUEST_PARAMS',
+        message: `Missing parameters in path params`,
+        details: '',
+        stack: '',
+        url: req.url
+      }
+    }
+    return new NextResponse(JSON.stringify(errorMessage), {
+      status: 400,
+      headers: { 'Content-Type': 'application/json' }
+    })
+  }
+
+  const userId: string | undefined = await getUserIdByWallet(walletId)
+  if (!userId) {
+    return NextResponse.json(
+      { error: { code: 'USER_NOT_FOUND', message: `User not found for wallet ${walletId}` } },
+      { status: 404, headers: { 'Content-Type': 'application/json' } }
+    )
+  }
+
+  return { walletId, userId }
+}
