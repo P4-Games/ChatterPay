581a64cbca209aef9d4a6a33812d8cd23601ec59
dappsar <dapps.ar@gmail.com>
2025-09-28 19:15:47 -0300

    [feat] :sparkles: add roadmap mock page (#258)

---
+++ src/app/roadmap/page.tsx
@@ -0,0 +1,11 @@
+import { RoadmapView } from 'src/sections/roadmap/view'
+
+// ----------------------------------------------------------------------
+
+export const metadata = {
+  title: 'ChatterPay: Roadmap'
+}
+
+export default function RoadmapPage() {
+  return <RoadmapView />
+}
