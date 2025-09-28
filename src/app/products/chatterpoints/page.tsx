bb1db5d82195f12a6321002bae348a168f8fc304
dappsar <dapps.ar@gmail.com>
2025-09-28 19:16:20 -0300

    [feat] :sparkles: add chatterpoints mock page (#259)

---
+++ src/app/products/chatterpoints/page.tsx
@@ -0,0 +1,11 @@
+import { ChatterPointsView } from 'src/sections/products/view/'
+
+// ----------------------------------------------------------------------
+
+export const metadata = {
+  title: 'ChatterPay: ChatterPoints'
+}
+
+export default function ProductsChatterPointsPage() {
+  return <ChatterPointsView />
+}
