2d5651bd2c367ac0456dbd720f9767c2080f56d7
dappsar <dapps.ar@gmail.com>
2026-08-30 01:09:54 -0300

    [feat] :sparkles: redirect to blocked page on user blocked error (#345)

--- src/app/api/hooks/api-resolver.ts
+++ src/app/api/hooks/api-resolver.ts
@@ -1,5 +1,7 @@
 import axios, { AxiosHeaders, type AxiosRequestConfig } from 'axios'
 
+import { paths } from 'src/routes/paths'
+
 import { BOT_API_URL, UI_BASE_URL } from 'src/config-global'
 
 // ----------------------------------------------------------------------
@@ -30,6 +32,13 @@ axiosInstance.interceptors.response.use(
       const errorCode = error.response?.data?.code
       if (typeof window !== 'undefined' && errorCode === 'NOT_AUTHORIZED') {
         window.dispatchEvent(new Event('auth:unauthorized'))
+      }
+      return Promise.reject(error)
+    }
+
+    if (error.response?.status === 403 && error.response?.data?.code === 'USER_BLOCKED') {
+      if (typeof window !== 'undefined' && window.location.pathname !== paths.pageBlocked) {
+        window.location.href = paths.pageBlocked
       }
       return Promise.reject(error)
     }
